2bec2f8ca70e1bbaf1fbefc9d06c852b9c1cd009
barnepallivishnuvardhan <112852299+barnepallivishnuvardhan@users.noreply.github.com>
2022-09-05 14:44:21 +0530

    Add files via upload

---
+++ 192111653 marks.py
@@ -0,0 +1,13 @@
+marks=int(input("enter a marks"))
+if (marks>=100):
+    print("grade s")
+elif("marks>=90"):
+    print("grade a")
+elif("marks>=80"):
+    print("grade b")
+elif("marks>=70"):
+    print("grade c")
+elif("marks>=60"):
+    print("grade d")
+else:
+    print("fail")
